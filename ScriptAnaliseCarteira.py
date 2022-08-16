194c8718e9f6b62a4e79b9330b57813ae07f6c7e
gabrielnrt <gabrielpv98@gmail.com>
2022-08-16 11:28:10 -0300

    Coloquei a entrada do arquivo como função.

--- ScriptAnaliseCarteira.py
+++ ScriptAnaliseCarteira.py
@@ -24,6 +24,57 @@ def conversor(fecha):
     return datetime.strptime(fecha, '%d/%m/%Y')
 
 
+#---------------------------------------------------------
+# Dados de entrada
+
+def LeituraDaCarteira():
+
+    char = input('Você possui uma carteira em .csv pronta? [s/n] ')
+
+    if char == 's':
+        arquivo = input('Digite o nome do arquivo incluindo o .csv: ')
+
+        separador = input('Digite o caractere que separa as colunas: ')
+
+        dec = input('Digite o caractere de decimal: ')
+
+        carteira = read_csv(arquivo,
+                            sep = separador,
+                            decimal = dec,
+                            parse_dates = ['Data da Compra'],
+                            date_parser = conversor)
+    else:
+        Ativos = []
+        Quantidades = []
+        Precos = []
+        Fechas = []
+
+        lampada = True
+        while lampada:
+            Ativos.append(input('\nDigite o código do ativo: '))
+
+            Quantidades.append(int(input('Digite a quantidade comprada: ')))
+
+            Precos.append(float(input('Digite o preço de compra da unidade: ')))
+
+            Fechas.append(input('Digite a data da compra no formato dd/mm/aaaa: '))
+
+            char = input('\nGostaria de inserir mais uma compra na carteira? [s/n]: ')
+
+            if char == 'n':
+                lampada = False
+
+                dicionario = {'Ativo':Ativos,
+                              'Quantidade':Quantidades,
+                              'Compra (R$)':Precos,
+                              'Data da Compra':Fechas}
+
+                carteira = DataFrame(dicionario,
+                                     parse_dates = ['Data da Compra'],
+                                     date_parser = conversor)
+    return carteira
+
+
 # Requisição API Yahoo
 
 def yahoo(ativo,tabela):
@@ -142,49 +193,7 @@ def graficos(df, nome):
 #-------------------------------------------------------
 # Arquivos de entrada
 
-char = input('Você possui uma carteira em .csv pronta? [s/n] ')
-
-if char == 's':
-    arquivo = input('Digite o nome do arquivo incluindo o .csv: ')
-
-    separador = input('Digite o caractere que separa as colunas: ')
-
-    dec = input('Digite o caractere de decimal: ')
-
-    carteira = read_csv(arquivo,
-                        sep = separador,
-                        decimal = dec,
-                        parse_dates = ['Data da Compra'],
-                        date_parser = conversor)
-else:
-    Ativos = []
-    Quantidades = []
-    Precos = []
-    Fechas = []
-
-    lampada = True
-    while lampada:
-        Ativos.append(input('\nDigite o código do ativo: '))
-
-        Quantidades.append(int(input('Digite a quantidade comprada: ')))
-
-        Precos.append(float(input('Digite o preço de compra da unidade: ')))
-
-        Fechas.append(input('Digite a data da compra no formato dd/mm/aaaa: '))
-
-        char = input('\nGostaria de inserir mais uma compra na carteira? [s/n]: ')
-
-        if char == 'n':
-            lampada = False
-
-            dicionario = {'Ativo':Ativos,
-                          'Quantidade':Quantidades,
-                          'Compra (R$)':Precos,
-                          'Data da Compra':Fechas}
-
-            carteira = DataFrame(dicionario,
-                                 parse_dates = ['Data da Compra'],
-                                 date_parser = conversor)
+carteira = LeituraDaCarteira()
 
 #-----------------------------------------------------------------------------------
 # Processamento de dados
